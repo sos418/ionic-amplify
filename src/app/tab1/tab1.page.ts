33d2a90792fc9edbe9a90d5eaa72bb33d6bac990
KuanYu <sos84418@gmail.com>
2021-07-03 15:57:24 +0800

    add amplify graph api and auth api

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { APIService } from '../API.service';
+
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,51 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public platform: Platform, public apiService: APIService) {
+
+
+    this.platform.resume.subscribe(async () => {
+      console.log('Resume event detected');
+    });
+
+    this.platform.resize.subscribe(async () => {
+      console.log('Resize event detected');
+    });
+
+    this.platform.pause.subscribe(async () => {
+      console.log('Pause event detected');
+    });
+
+  }
+
+  todos: Array<any>;
+
+
+  createTodo() {
+    this.apiService.CreateTodo({
+        name: 'ionic',
+        description: 'testing'
+    });
+  }
+
+  getTodo() {
+    this.platform.ready().then(() => {
+      //other code
+      this.apiService.ListTodos().then((evt) => {
+        this.todos = evt.items;
+      });
+    });
+  }
+
+  alertTodo() {
+    alert(JSON.stringify(this.todos));
+  }
+
+  // logTodo() {
+  //   this.apiService.OnCreateTodoListener.subscribe((evt) => {
+  //     const data = (evt as any).value.data.onCreateTodo;
+  //     this.todos = [...this.todos, data];
+  //   });
+  // }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Auth } from 'aws-amplify';
 
 @Component({
   selector: 'app-tab2',
@@ -8,5 +9,49 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   constructor() {}
+
+
+  async signUp() {
+    const username : string = 'Sam';
+    const password : string = '1qaz!QAZ';
+    const email : string = 'sos84418@gmail.com';
+    const phone_number : string = '+886978822075';
+      try {
+          const { user } = await Auth.signUp({
+              username,
+              password,
+              attributes: {
+                  email,          // optional
+                  phone_number,   // optional - E.164 number convention
+                  // other custom attributes 
+              }
+          });
+          console.log('!!! SignUp:',JSON.stringify(user));
+      } catch (error) {
+          console.log('error signing up:', JSON.stringify(error));
+      }
+  }
+
+  async  confirmSignUp() {
+    try {
+      const username : string = 'sam';
+      const code : string = '585496';
+      await Auth.confirmSignUp(username, code);
+    } catch (error) {
+        console.log('error confirming sign up', JSON.stringify(error));
+    }
+  }
+
+  async signIn() {
+    const username : string = 'sam';
+    const password : string = '1qaz!QAZ';
+    try {
+        const user = await Auth.signIn(username, password);
+        console.log('!!! success signing in', JSON.stringify(user));
+    } catch (error) {
+        console.log('error signing in', JSON.stringify(error));
+    }
+  }
+
 
 }
